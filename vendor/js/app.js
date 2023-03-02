2b7d74461ae638eb029598bc011d72abcb9f2f47
Mateus Fernandes <mateusowlfernandes@gmail.com>
2023-03-02 16:51:27 -0300

    Created a image slider, but the sliding function does not work. I'm gonna solve it later on

--- vendor/js/app.js
+++ vendor/js/app.js
@@ -103,3 +103,73 @@ form.addEventListener('submit', (e) => {
   checkLength(passwordConfirm, 8, 64);
   passwordMatch(password, passwordConfirm);
 });
+
+//Image Gallery
+let galleryImages = document.querySelectorAll('.services-cell');
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+galleryImages.forEach(function (image, index) {
+  image.onclick = function () {
+    getLatestOpenedImg = index + 1;
+
+    let container = document.body;
+    let newImgWindow = document.createElement('div');
+    container.appendChild(newImgWindow);
+    newImgWindow.setAttribute('class', 'img-window');
+    newImgWindow.setAttribute('onclick', 'closeImg()');
+
+    let newImg = image.firstElementChild.cloneNode();
+    newImgWindow.appendChild(newImg);
+    newImg.classList.remove('services-cell-img');
+    newImg.classList.add('popup-img');
+    newImg.setAttribute('id', 'current-img');
+
+    newImg.onload = function () {
+      let newNextBtn = document.createElement('a');
+      newNextBtn.innerHTML = '<i class="fas fa-chevron-right next"></i>';
+      container.appendChild(newNextBtn);
+      newNextBtn.setAttribute('class', 'img-btn-next');
+      newNextBtn.setAttribute('onclick', 'change-img(1)');
+
+      let prevNextBtn = document.createElement('a');
+      prevNextBtn.innerHTML = '<i class="fas fa-chevron-left next"></i>';
+      container.appendChild(prevNextBtn);
+      prevNextBtn.setAttribute('class', 'img-btn-prev');
+      prevNextBtn.setAttribute('onclick', 'change-img(0)');
+    };
+  };
+});
+
+function closeImg() {
+  document.querySelector('.img-window').remove();
+  document.querySelector('.img-btn-next').remove();
+  document.querySelector('.img-btn-prev').remove();
+}
+
+function changeImg(change) {
+  document.querySelector('#current-img').remove();
+
+  let getImgWindow = document.querySelector('.img-window');
+  let newImg = document.createElement('img');
+  getImgWindow.appendChild(newImg);
+
+  let calcNewImg;
+  if (change === 1) {
+    calcNewImg = getLatestOpenedImg + 1;
+    if (calcNewImg > galleryImages.length) {
+      calcNewImg = 1;
+    }
+  } else if (change === 0) {
+    calcNewImg = getLatestOpenedImg - 1;
+    if (calcNewImg < 1) {
+      calcNewImg = galleryImages.length;
+    }
+  }
+
+  newImg.setAttribute('scr', '/vendor/img/img-' + calcNewImg + '.jpg');
+  newImg.setAttribute('class', 'popup-img');
+  newImg.setAttribute('id', 'current-img');
+
+  getLatestOpenedImg = calcNewImg;
+}
